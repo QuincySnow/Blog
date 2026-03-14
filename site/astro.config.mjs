dea3569399bd40ae8206348e13c69911b449ed1c
QuincySnow <146918156+QuincySnow@users.noreply.github.com>
2026-03-14 12:09:05 +0800

    Correct base path in Astro configuration from '/Blog' to '/blog'

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -7,6 +7,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https:/quincysnow.github.io',
-	base: '/Blog',
+	base: '/blog',
 	integrations: [mdx(), sitemap()],
 });
